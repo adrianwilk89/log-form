ab8c81f7057cc999c00e51e8037f04bf2dc91e44
Adrian <adrian.przybyla@o2.pl>
2023-03-21 02:02:58 +0100

    improve logs

--- api/db.js
+++ api/db.js
@@ -1,6 +1,8 @@
 const PrismaClient = require('@prisma/client').PrismaClient
 
-const client = new PrismaClient();
+const client = new PrismaClient({
+    errorFormat: 'pretty',
+});
 
 const initDb = async () => {
     const result = await client.$queryRaw `SELECT 1=1 AS "database ready";`;
